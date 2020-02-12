858509c87ceff38fd34032eb8a52bc7fc7498623
Sang <sangnguyen5697@gmail.com>
2020-02-11 23:39:03 -0500

    push to github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyNgIfDirective } from './my-ng-if.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNgIfDirective
   ],
   imports: [
     BrowserModule
